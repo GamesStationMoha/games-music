2bdb284b5b38828639dfcd219ac075ce98362b6c
GamesStationMoha <45317067+GamesStationMoha@users.noreply.github.com>
2018-11-24 20:23:23 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.TOKEN = 'NTE1OTUzNDMwMjM0MjY3NjQ4.Dtsofg.Etu8jOCbwe57uKc3UBK-LxfppAs';
 
-exports.PREFIX = '!';
+exports.PREFIX = '1';
 
 exports.GOOGLE_API_KEY = 'AIzaSyDWE7m3F-p4CUT2lv7oudtY5r2ryPNehXE';
